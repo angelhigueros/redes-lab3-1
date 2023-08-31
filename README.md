7be2255d8b82d5cd1878592ea39792c652079efc
Angel Higueros <angel.higueros01@gmail.com>
2023-08-31 01:34:06 -0600

    feat(): dijkstra

---
+++ README.md
@@ -0,0 +1,9 @@
+# LABORATORIO 3.1
+### Integrantes
+- Fredy Velasquez
+- Mariana David
+- Angel Higueros
+
+### Uso
+- ´npm run dijkstra´
+- ´npm run floading
--- dijkstra.js
+++ dijkstra.js
@@ -1,4 +1,79 @@
-const { generateRandomGraph } = require('./weightedDirectedGraph.js')
+const readline = require('readline')
+
+class WeightedDirectedGraph {
+  constructor() {
+    this.nodes = {}
+  }
+
+  addNode(node) {
+    if (!this.nodes[node]) {
+      this.nodes[node] = {}
+    }
+  }
+
+  addEdge(node1, node2, weight) {
+    this.addNode(node1)
+    this.addNode(node2)
+    this.nodes[node1][node2] = weight
+  }
+
+  getNeighbors(node) {
+    return Object.keys(this.nodes[node])
+  }
+
+  getWeight(node1, node2) {
+    return this.nodes[node1][node2]
+  }
+
+  send(path, type, from, to, payload) {
+    for (let i = 0; i < path.length; i++) {
+      let data = {
+        type,
+        headers: { from, to, hop_count: i },
+        payload: payload,
+      }
+      const node = path[i]
+
+      if (node === to) {
+        console.log(payload)
+      } else {
+        console.log(data)
+      }
+    }
+  }
+}
+
+// Create the tree auto
+function generateRandomGraph(nodes, edges) {
+  const graph = new WeightedDirectedGraph()
+
+  for (let i = 0; i < nodes.length; i++) {
+    graph.addNode(nodes[i])
+  }
+
+  for (let i = 0; i < edges; i++) {
+    const node1 = nodes[Math.floor(Math.random() * nodes.length)]
+    const node2 = nodes[Math.floor(Math.random() * nodes.length)]
+    const weight = Math.floor(Math.random() * 10) + 1 // random weight between 1 and  10
+    graph.addEdge(node1, node2, weight)
+  }
+
+  return graph
+}
+
+// Helpers
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+const askQuestion = question => {
+  return new Promise(resolve => {
+    rl.question(question, answer => {
+      resolve(answer)
+    })
+  })
+}
 
 const dijkstra = (graph, start, end) => {
   let distances = {}
@@ -45,19 +120,30 @@ const dijkstra = (graph, start, end) => {
   return path.reverse()
 }
 
-// Create graph
-const nodes = ["A", "B", "C", "D", "E", "F", "G", "H"];
-const numberOfEdges = 10;
-const graph = generateRandomGraph(nodes, numberOfEdges)
+const main = async () => {
+  console.log(':: LAB 3 - DIJKSTRA ::\n')
 
-// Show graph
-console.log("Graph");
-console.log(graph.nodes);
+  // Create graph
+  const nodes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+  const numberOfEdges = 12
+  const graph = generateRandomGraph(nodes, numberOfEdges)
 
+  // Show graph
+  console.log('Graph')
+  console.log(graph.nodes)
 
-// Simulate conection between nodes and find the shortest path
-const startNode = 'A'
-const endNode = 'G'
-const shortestPath = dijkstra(graph, startNode, endNode)
+  console.log('\nSEND MESSAGE')
+  const from = await askQuestion('From: ')
+  const to = await askQuestion('To: ')
+  const message = await askQuestion('Message: ')
+  rl.close()
 
-console.log(`Shortest path from ${startNode} to ${endNode}`, shortestPath)
+  // Tabla de enrutamiento
+  const shortestPath = dijkstra(graph, from, to)
+  console.log(`Shortest path from ${from} to ${to}`, shortestPath)
+
+  // Envío de “paquetes”
+  graph.send(shortestPath, 'message', from, to, message)
+}
+
+main()
